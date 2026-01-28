628266d8614c7a46619ec076ffe2153fa163f264
Timothy Tulang <tulangtimothy@gmail.com>
2026-01-28 10:49:39 +0800

    Fixed CORS blocking issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://soluna-web-theta.vercel.app/"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://soluna-web-theta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
