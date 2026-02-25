0b584017dd0ed9e548f81df9a0f985e0ed8f98af
reinbrant <reiignftrs@gmail.com>
2026-02-25 10:24:21 +0800

    Fixed delims

--- backend/app/lexer/delims.py
+++ backend/app/lexer/delims.py
@@ -17,7 +17,7 @@ DELIMS = {
     'semicolon_delim': {*REGDEF['alphabet'], *REGDEF['free_delim']},
     'string_concat_delim': {'(', '"', '\'', '_', *REGDEF['alphanumeric'], *REGDEF['free_delim']},
     'string_length_delim': {'(', '"', '\'', '_', *REGDEF['alphabet']},
-    'unary_delim': {'(', '*', '%', '/', *REGDEF['relational_operators'], ')', ';', '&', '|', '_', '=', *REGDEF['alphanumeric'], *REGDEF['free_delim']},
+    'unary_delim': {'(', '*', '%', '/', *REGDEF['relational_operators'], ')', ';', '&', '|', '_', '=', ',', *REGDEF['alphanumeric'], *REGDEF['free_delim']},
     'identifier_delim': {*REGDEF['free_delim'], *REGDEF['arithmetic_operators'], *REGDEF['relational_operators'], '(', ')', '[' , ']', '}', '.', ';', ',', '&', '|', '='},
     'warp_delim': {';'},
     'open_bracket_delim': {'"', '\'', '-', '(' , '{', '}', '+', *REGDEF['alphanumeric'], *REGDEF['free_delim']},
